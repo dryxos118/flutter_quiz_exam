50683308178d23526605753b1645349d3c81fced
alexis <alexishu59134@gmail.com>
2024-10-28 16:58:35 +0100

    :sparkles: A editor of quiz TODO : :recycle: REFACTO

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -16,6 +16,20 @@ class Question {
     required this.correctAnswerIndex,
   });
 
+  Question copyWith({
+    String? text,
+    List<String>? options,
+    int? correctAnswerIndex,
+    bool? isTrueFalse,
+  }) {
+    return Question(
+      text: text ?? this.text,
+      options: options ?? this.options,
+      correctAnswerIndex: correctAnswerIndex ?? this.correctAnswerIndex,
+      isTrueOrFalse: isTrueFalse ?? isTrueOrFalse,
+    );
+  }
+
   factory Question.fromJson(Map<String, dynamic> json) =>
       _$QuestionFromJson(json);
   Map<String, dynamic> toJson() => _$QuestionToJson(this);
