b1cab4ef384f3f38727435f862eff916c1a1a1cd
alexis <alexishu59134@gmail.com>
2024-10-31 10:38:42 +0100

    Fix last time

--- lib/views/profile/last_quizzes.dart
+++ lib/views/profile/last_quizzes.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz_exam/constants/constants.dart';
-import 'package:flutter_quiz_exam/layout/quiz_bottom_navigation.dart';
 import 'package:flutter_quiz_exam/logic/provider/user_provider.dart';
-import 'package:flutter_quiz_exam/models/leaderboard.dart';
 import 'package:flutter_quiz_exam/views/profile/last_quiz_card.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LastQuizzes extends ConsumerWidget {
